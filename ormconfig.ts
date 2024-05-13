fe048f9326e5ade02c740311beb2015d368568ee
You_5C24 <youlongte@fhcn.cn>
2024-05-13 14:55:58 +0800

    feat: 事务

---
+++ ormconfig.ts
@@ -0,0 +1,11 @@
+module.exports = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'pass123',
+  database: 'postgres',
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/src/migrations/*.js'],
+  migrationsTableName: 'migration',
+};
